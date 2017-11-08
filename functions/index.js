0613be29a679f4c8a35a15c1b09fb586aa723f60
NGUYEN NGOC KIEN <keyyuki@gmail.com>
2017-11-08 10:20:12 +0700

    authen setup

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
-var functions = require('firebase-functions');
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({origin: true});
+const app = express();
+const AuthenticatePlugin = require('./AuthenticatePlugin');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
@@ -6,3 +15,14 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // })
+app.use(cors);
+app.use(cookieParser);
+app.use(AuthenticatePlugin);
+app.get('/hello', (req, res) => {
+    res.send(`Hello ${req.user.name}`);
+});
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.app = functions.https.onRequest(app);
